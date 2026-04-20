aac93310e31daeedeefad2b0235844bd135a7267
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 01:59:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-
+ 
 terraform {
   backend "s3" {
     bucket         = "otms-dev-state7864582"
@@ -10,14 +10,16 @@ terraform {
     dynamodb_table = "terraform-lock"
   }
 }
-
+ 
 resource "aws_vpc" "main_vpc" {
-  cidr_block = var.vpc_cidr
-
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-
+ 
   tags = {
-    Name = var.vpc_name
+    Name        = var.vpc_name
+    Environment = "dev"
+    Project     = "OTMS"
+    ManagedBy   = "Terraform"
   }
 }
