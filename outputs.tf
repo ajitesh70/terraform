e155c879f6863c24c3134c53037ea9417bdaaa7f
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 02:00:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "vpc_id" {
   description = "ID of the created VPC"
   value       = aws_vpc.main_vpc.id
 }
-
+ 
 output "vpc_cidr" {
   description = "CIDR block of the VPC"
   value       = aws_vpc.main_vpc.cidr_block
