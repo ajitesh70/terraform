59199d8fb15045beb874acf54d2b5fb7c2153505
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 01:59:08 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,13 +3,13 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-
+ 
 variable "bucket_name" {
   description = "S3 bucket name for Terraform state"
   type        = string
   default     = "otms-dev-state7864582"
 }
-
+ 
 variable "dynamodb_table" {
   description = "DynamoDB table for state locking"
   type        = string
