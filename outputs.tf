1da7a53895886596c53d01eafca22decce688ac9
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 15:50:59 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "launch_template_id" {
-  value = aws_launch_template.app_template.id
+  description = "Launch Template ID"
+  value       = aws_launch_template.app_template.id
 }
-
+ 
 output "launch_template_latest_version" {
-  value = aws_launch_template.app_template.latest_version
+  description = "Latest version number of the Launch Template"
+  value       = aws_launch_template.app_template.latest_version
 }
