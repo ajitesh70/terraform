a7aaa94258af2a30e002c9d5eebc7bfc7efd7eb4
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 15:45:10 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "public_route_table_id" {
-  value = aws_route_table.public_rt.id
+  description = "Public route table ID"
+  value       = aws_route_table.public_rt.id
 }
-
+ 
 output "private_route_table_id" {
-  value = aws_route_table.private_rt.id
+  description = "Private route table ID"
+  value       = aws_route_table.private_rt.id
 }
