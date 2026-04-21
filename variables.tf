3b13e21444c3aea87fc7a8b06a6e78ec7a8f234b
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 18:09:54 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "env" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+  default     = "otms"
+}
+
+variable "min_size" {
+  description = "Minimum number of instances in ASG"
+  type        = number
+  default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in ASG"
+  type        = number
+  default     = 3
+}
+
+variable "desired_capacity" {
+  description = "Desired number of instances in ASG"
+  type        = number
+  default     = 1
+}
