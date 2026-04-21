e56c7e63c8e09ae55bef9dd807d2018dae7d9422
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 15:46:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "aws_region" {
   description = "AWS Region"
+  type        = string
   default     = "us-east-1"
 }
-
+ 
 variable "key_name" {
   description = "Name of the SSH key pair"
-  default     = "my-terraform-key"
+  type        = string
+  default     = "otms-dev-key"
 }
