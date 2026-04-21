c33b3344a60a97ff295868e645b572f88a834d4a
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 15:43:40 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "nat_gateway_id" {
   description = "NAT Gateway ID"
   value       = aws_nat_gateway.nat_gw.id
 }
-
+ 
 output "nat_public_ip" {
   description = "Elastic IP of NAT Gateway"
   value       = aws_eip.nat_eip.public_ip
