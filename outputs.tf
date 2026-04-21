4d00c6060d661e3d0d14f16608fa16baa4b55815
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 15:46:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "key_name" {
-  value = aws_key_pair.generated_key.key_name
+  description = "Name of the SSH Key Pair"
+  value       = aws_key_pair.generated_key.key_name
 }
-
+ 
 output "private_key_file" {
-  value = local_file.private_key_pem.filename
+  description = "Local path of saved private key PEM"
+  value       = local_file.private_key_pem.filename
 }
