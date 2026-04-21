cbc3e02ab6d48ea2ee6d35bc44ae204bafa6c1c9
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 17:56:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "env" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+  default     = "otms"
+}
+
+variable "domain_name" {
+  description = "Primary domain name for hosted zone (e.g. otms.yourdomain.com)"
+  type        = string
+  default     = "www.innovitisolutions.in"
+}
