e70abc5d37c76597d664e224f354a4b25316bf94
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-21 15:46:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,38 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
-
+ 
 terraform {
   backend "s3" {
-    bucket = "otms-dev-state7864582"
-    region = "us-east-1"
-    key    = "env/dev/application/network/sshkey/terraform.tfstate"
+    bucket         = "otms-dev-state7864582"
+    key            = "env/dev/application/network/sshkey/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
   }
 }
-
-
-# Generate private key
+ 
+# Generate RSA Private Key
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-
-# Create AWS Key Pair using public key
+ 
+# Create AWS Key Pair using generated public key
 resource "aws_key_pair" "generated_key" {
   key_name   = var.key_name
   public_key = tls_private_key.ssh_key.public_key_openssh
+ 
+  tags = {
+    Name        = var.key_name
+    Environment = "dev"
+    Project     = "OTMS"
+    ManagedBy   = "Terraform"
+  }
 }
-
+ 
 # Save private key locally
 resource "local_file" "private_key_pem" {
-  content  = tls_private_key.ssh_key.private_key_pem
-  filename = "${var.key_name}.pem"
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${var.key_name}.pem"
+  file_permission = "0400"
 }
