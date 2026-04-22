37c13ed3b43ee37acee7749953e245957340b1ad
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-22 23:27:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,19 @@
 variable "region" {
-  description = "AWS Region"
-  type        = string
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "env" {
-  description = "Deployment environment"
-  type        = string
-  default     = "dev"
+  default = "dev"
 }
 
 variable "project" {
-  description = "Project name"
-  type        = string
-  default     = "otms"
+  default = "otms"
 }
 
 variable "ami_id" {
-  description = "AMI ID for Notification service"
-  type        = string
-  default     = "ami-0b73ce37f347c345b"
+  default = "ami-0b73ce37f347c345b"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.small"
+  default = "t3.small"
 }
