eb6a4c229579f9054c21e422e18e2df94eb9ee2a
Ajitesh Singh <ajitesh@example.com>
2026-05-03 02:05:46 +0530

    Update frontend AMI to re-encrypted version

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "region" {
 variable "ami_id" {
   description = "AMI ID for Launch Template"
   type        = string
-  default     = "ami-033bd18826652770f"
+  default     = "ami-03f5347d027ee35a1"
 }
  
 variable "instance_type" {
