03ecf3eb23ab9e85e5b6b2cd01f9f800e4b68aac
Ajitesh Singh <ajitesh@example.com>
2026-05-03 02:08:47 +0530

    Update frontend EC2 AMI to re-encrypted version

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "project" {
 variable "ami_id" {
   description = "AMI ID for frontend EC2 instance"
   type        = string
-  default     = "ami-033bd18826652770f"
+  default     = "ami-03f5347d027ee35a1"
 }
 
 variable "instance_type" {
