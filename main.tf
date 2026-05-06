9efb0f829a40109c8ce68c6496b317c2662b5e69
Ajitesh Singh <ajitesh@example.com>
2026-05-06 22:37:17 +0530

    Fix: use frontend SG instead of ALB SG in launch template

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = var.region
 }
- 
 terraform {
   backend "s3" {
     bucket         = "otms-dev-state7864582"
@@ -10,49 +9,38 @@ terraform {
     dynamodb_table = "terraform-lock"
   }
 }
- 
 # GET SUBNET FROM REMOTE STATE
 data "terraform_remote_state" "subnet" {
   backend = "s3"
- 
   config = {
     bucket = "otms-dev-state7864582"
     key    = "env/dev/application/network/subnet/terraform.tfstate"
     region = "us-east-1"
   }
 }
- 
-# GET EXTERNAL ALB SG FROM REMOTE STATE
-data "terraform_remote_state" "alb_sg" {
+# GET FRONTEND SG FROM REMOTE STATE
+data "terraform_remote_state" "frontend_sg" {
   backend = "s3"
- 
   config = {
     bucket = "otms-dev-state7864582"
-    key    = "env/dev/application/otms/external-alb/terraform.tfstate"
+    key    = "env/dev/application/otms/frontend-sg/terraform.tfstate"
     region = "us-east-1"
   }
 }
- 
 # Launch Template
 resource "aws_launch_template" "app_template" {
   name_prefix   = "${var.project}-${var.env}-template-"
   image_id      = var.ami_id
   instance_type = var.instance_type
- 
-  # SSH Key attached
-  key_name = var.key_name
- 
+  key_name      = var.key_name
   vpc_security_group_ids = [
-    data.terraform_remote_state.alb_sg.outputs.security_group_id
+    data.terraform_remote_state.frontend_sg.outputs.frontend_sg_id
   ]
- 
   monitoring {
     enabled = true
   }
- 
   tag_specifications {
     resource_type = "instance"
- 
     tags = {
       Name        = "${var.project}-${var.env}-app"
       Environment = var.env
