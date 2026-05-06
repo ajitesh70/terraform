b83c85520ffec8914fccec01beda854dbd4d6e97
Ajitesh Singh <ajitesh@example.com>
2026-05-07 00:27:53 +0530

    Fix: update API AMI to ami-017282e4f588af446

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "project" {
 variable "ami_id" {
   description = "AMI ID"
   type        = string
-  default     = "ami-0b73ce37f347c345b"
+  default     = "ami-017282e4f588af446"
 }
 
 variable "instance_type" {
