36540c9510bf00303b595c1db9ed55110c1ed2a1
Ajitesh Singh <ajitesh@example.com>
2026-05-11 14:27:51 +0530

    Fix: correct SSH key remote state path

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ data "terraform_remote_state" "ssh_key" {
   backend = "s3"
   config = {
     bucket = "otms-dev-state7864582"
-    key    = "env/dev/application/network/ssh-key/terraform.tfstate"
+    key    = "env/dev/application/network/sshkey/terraform.tfstate"
     region = "us-east-1"
   }
 }
